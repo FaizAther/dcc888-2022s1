ead2a178c7662b0e4d011634e9f4fdd50a820761
FaizAther <faizather@protonmail.com>
2022-04-24 11:05:21 +1000

    instructions closer to spec

--- Ufmg.cpp
+++ Ufmg.cpp
@@ -6,6 +6,7 @@
 #include "llvm/IR/Function.h"
 #include "llvm/IR/BasicBlock.h"
 #include "llvm/IR/CFG.h"
+#include "llvm/IR/Constants.h"
 #include <vector>
 
 #include "llvm/IR/LegacyPassManager.h"
@@ -39,12 +40,50 @@ namespace {
     }
 
     bool runOnBasicBlock(BasicBlock *bb, raw_fd_ostream *stream) {
-      *stream << "\tNode" << (void *)bb << "[shape=record, label=\"{";
+      *stream << "\tNode" << (void *)bb << "[shape=record, label=\"{BB";
       bb->printAsOperand(*stream, false);
       *stream << ":";
       for (BasicBlock::InstListType::iterator il = bb->getInstList().begin(), e = bb->getInstList().end(); e != il; il++) {
         Instruction *itn = &*il;
-        *stream << "\\l  " << *itn;
+        if (itn->getOpcode() == Instruction::Alloca || itn->getOpcode() == Instruction::Store) {
+          continue;
+        }
+        // *stream << "\\l  " << *itn;
+        *stream << "\\l  ";
+        if (itn->getOpcode() != Instruction::Ret && itn->getOpcode() != Instruction::Br) {
+          itn->printAsOperand(*stream, false);
+          *stream << itn->getName() << " = ";
+        }
+        *stream << itn->getOpcodeName() << " ";
+        // if (itn->getOpcode() == Instruction::Call) {
+        //   errs() << "Is a function call\n" << *itn << "\n";
+        // }
+        for (unsigned int i = 0; i < itn->getNumOperands(); i++) {
+          Value *iop = itn->getOperand(i);
+          if (itn->getOpcode() == Instruction::Call) {
+            if (iop->hasName()) {
+              *stream << "@";
+              *stream << iop->getName();
+            } else {
+              if (auto *CstExpr = dyn_cast<ConstantExpr>(itn->getOperand(i))) {
+                // BitCastInst is an *Instrution*, here you have a *ConstantExpr* Bitcast
+                if (CstExpr->isCast()) {
+                   *stream << "@";
+                   *stream << CstExpr->getOperand(0)->getName();
+                }
+              } else { // Unnamed
+                *stream << iop->getNameOrAsOperand();
+              }
+            }
+          } else {
+            iop->printAsOperand(*stream, false);
+            // *stream << " " << *itn->getOperand(i);
+          }
+          if (i != itn->getNumOperands() - 1) {
+            *stream << ", ";
+          }
+          // *stream << " " << *itn->getOperand(i);
+        }
       }
       *stream << "\\l}\"];\n";
       return false;
